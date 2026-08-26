6a43cb2cc9cbcba249eb7e87bc20fd467110d12a
Juan Luna <JuanLuna020205@gmail.com>
2026-08-26 22:31:23 +0000

    Primera introduccion del proyecto + opciones 1, 2 y 6 + -d

---
+++ proyecto1_bash.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+export FILENAME="alumnos"
+
+if [ "$1" = "-d" ]
+then
+    PID=`ps -ef | grep consolidar.sh | grep -v grep | awk '{print $2}'`
+
+    if [ "$PID" != "" ]
+    then
+	kill $PID
+    fi
+
+    rm -r -f ~/EPNro1
+
+    echo "Eliminado y procesos finalizados"
+    exit
+fi
+
+echo -e "MENU\nQue Queres Hacer?"
+echo "1) Crear Entorno"
+echo "2) Correr Proceso"
+echo "3) Mostrar Alumnos Ordenados por Padron"
+echo "4) Mostrar las 10 Notas mas Altas"
+echo "5) Buscar Alumno por Padron"
+echo "6) Visualizar log"
+echo "7) Salir"
+
+echo "Ingrese una opcion del 1 al 7:"
+read opcion
+
+while [ $opcion -lt 1 ] || [ $opcion -gt 7 ]
+do
+	echo "Opcion invalida. Ingrese una opcion del 1 al 7:"
+	read opcion
+done
+
+case $opcion in
+
+	1)
+	   mkdir -p ~/EPNro1/entrada
+	   mkdir -p ~/EPNro1/salida
+	   mkdir -p ~/EPNro1/procesado
+
+       	   cp ~/consolidar.sh ~/EPNro1/consolidar.sh
+
+	  echo "Entorno Creado Correctamente"
+	  ;;
+
+	2)
+	   touch ~/EPNro1/procesado.log
+
+	   if pgrep -f  consolidar.sh
+	   then
+		echo "El proceso consolidar.sh ya esta ejecutandose"
+	   else
+		 bash ~/EPNro1/consolidar.sh &
+		 echo "Proceso iniciado en background"
+	   fi
+	   ;;
+
+	6)
+
+	   if [ -f ~/EPNro1/procesado.log ]
+	   then
+		 cat ~/EPNro1/procesado.log
+	    else
+		 echo "El archivo de log no existe"
+	   fi
+	   ;;
+
+esac
+
